ca90f38c88f03b96c98ae565514e9556aef00c1e
olliw42 <waldmanns@gmx.de>
2018-08-02 14:10:44 +0200

    compiled v2,v3,v4, APM_Config.h to make v2 fit, v4 bench tested, v3 solo flight tested issue with solo heartbeat

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -28,6 +28,8 @@ features:
  - Solo gimbal mimicry
 
 changed files:
+    APM_Config.h: low flash for v2, said to be not used anymore, but obviously still works
+
     AP_Camera.cpp: 2x
     AP_HAL_PX4 UARTDriver.cpp: 3x
     AP_HAL_PX4 UARTDriver.h: 2x
@@ -38,4 +40,10 @@ changed files:
     GCS_Common.cpp: 4x
     GCS.cpp: 1x
     GCS.h: 3x
+
+added files:
+    BP_Mount_STorM32.cpp
+    BP_Mount_STorM32.h
+    STorM32_lib.cpp
+    STorM32_lib.h
 */
