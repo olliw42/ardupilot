8c482e5de9ae3e2d040ae97dafe4e311ece8a291
olliw42 <waldmanns@gmx.de>
2018-11-10 17:48:29 +0100

    version bump to v0.15u

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -8,7 +8,7 @@
 
 //OW
 //#define THISFIRMWARE "ArduCopter V3.6.1"
-#define THISFIRMWARE "BetaCopter V3.6.1 v014u"
+#define THISFIRMWARE "BetaCopter V3.6.1 v015u"
 //OW
 
 // the following line is parsed by the autotest scripts
